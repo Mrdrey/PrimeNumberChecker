11936a3b27f3e2dfd78497e61b75df5cc6e22f9a
drey <dreybrizuela12@gmail.com>
2025-01-22 22:24:07 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+
+// I declare the input and output by using dom Element
+const input = document.getElementById('inputNumber');
+const textResult = document.getElementById('textResult');
+const numberResult = document.getElementById('numberResult');
+const error = document.getElementById('error');
+// buttons for submit and clear input and outputs
+const submitBtn = document.getElementById('submit');
+const clearBtn = document.getElementById('clearBtn');
+
+// this function is for clearing the input and output
+clearBtn.addEventListener('click',()=>{
+    textResult.innerText = '';
+    numberResult.innerText = '';
+    input.value = '';
+    error.innerText = '';
+}) 
+// this function is for checking if the input number is prime or not
+function primeNumber(){
+    
+    if(input.value % 2 == 0){
+        textResult.innerHTML = "Not a Prime Number"
+    }
+    else{
+        textResult.innerHTML = "Prime Number"
+    }
+}
+
+/**
+ * Calculates the factorial of a given number and updates the inner HTML of an element with id 'numberResult'.
+ * 
+ * @param {number} num - The number to calculate the factorial of.
+ */
+
+function factorial(num){
+    let result = 1;
+    // loop through the number and get the factorial
+    for(let i = 1;i<num; i++){
+      
+        result*= num;
+        
+    }
+    // display the result of the factorial
+    numberResult.innerHTML = result
+}
+// this function is for displaying the result of the prime number and the factorial of the input number
+submitBtn.addEventListener('click',(event)=>{
+    event.preventDefault();
+
+   try{
+    // check if the input is not a number
+    if(isNaN(input.value)){
+        error.innerHTML = "Invalid Input"
+    }
+    // check if the input is empty
+    else if(input.value == ""){
+        error.innerHTML = "Please Enter a Number"
+    }
+    // if the input is correct
+    else{
+        primeNumber();
+        factorial(input.value)
+        error.innerHTML = "";
+    }
+   }
+    // catch the error and display the error message
+   catch(error)
+   {
+    textResult.innerHTML = error.message;
+   }
+})
+
+
